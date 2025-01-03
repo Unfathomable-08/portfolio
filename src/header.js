d189bcae61f438a7aa59bbf3a7f79a8aaac11dce
Muhammad <muhammad124711@gmail.com>
2025-01-03 03:23:44 -0800

    fixed header

--- src/header.js
+++ src/header.js
@@ -2,8 +2,8 @@ const Header = (props) => {
     return (
         <div className="header fixed z-10 w-full h-[70px] bg-tertiary flex items-center">
             <div className="container flex justify-between">
-                <h1 className="text-2xl pb-1 px-20 font-medium">Portfolio.</h1>
-                <nav className="px-5">
+                <h1 className="text-2xl pb-1 px-8 md:px-20 font-medium">Portfolio.</h1>
+                <nav className="px-5 invisible md:visible">
                     <ul className="flex text-lg">
                         <li className={props.active === 'hero' ? 'active px-5' : 'px-5'}><a href="#home">Home</a></li>
                         <li className={props.active === 'about' ? 'active px-5' : 'px-5'}><a href="#about">About</a></li>
