2b547b50adb941ea849a92cc26dfdc805ecb7c72
Muhammad <muhammad124711@gmail.com>
2026-07-13 23:55:23 +0500

    added og sqaure

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,6 +16,20 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+const personSchema = {
+  "@context": "https://schema.org",
+  "@type": "Person",
+  name: "Muhammad",
+  url: "https://dev-muhammad.vercel.app",
+  image: "https://dev-muhammad.vercel.app/og-square.png",
+  sameAs: [
+    "https://github.com/Unfathomable-08",
+    "https://x.com/unfathomable_08",
+    "https://instagram.com/unfathomable_08",
+    "https://linkedin.com/in/muhammad-abasi-6b6884350"
+  ]
+};
+
 export const metadata = {
   title: "Muhammad Portfolio | Fullstack Web & Applied AI Developer",
   description: "Explore My professional portfolio, featuring full-stack projects, AI tools, and modern web apps. Built with Next.js and passion.",
@@ -33,6 +47,12 @@ export const metadata = {
         height: 630,
         alt: "Muhammad Portfolio",
       },
+      {
+        url: "https://dev-muhammad.vercel.app/og-square.png",
+        width: 1200,
+        height: 1200,
+        alt: "Muhammad Portfolio",
+      }
     ],
   },
   twitter: {
@@ -52,9 +72,9 @@ export default function RootLayout({ children }) {
       <head>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
         {/* Google Analytics */}
-        <Script 
-          src="https://www.googletagmanager.com/gtag/js?id=G-XLRTPWED97" 
-          strategy="afterInteractive" 
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-XLRTPWED97"
+          strategy="afterInteractive"
         />
         <Script id="google-analytics" strategy="afterInteractive">
           {`
@@ -65,6 +85,12 @@ export default function RootLayout({ children }) {
             gtag('config', 'G-XLRTPWED97');
           `}
         </Script>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify(personSchema),
+          }}
+        />
         {/* Google Search Console Verification */}
         <meta name="google-site-verification" content="Y7EkvICXUQUp40MdDpDtw1lN_Pwn1rHZK_K5ZG1SKeo" />
       </head>
